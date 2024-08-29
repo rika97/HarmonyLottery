3e21a6a3328fe37257cb7c25eda6ebd5d4c78c1e
Rika <15hatoyar@gmail.com>
2024-08-28 21:07:16 -0700

    ui edit

--- client/src/components/PointsBalance.js
+++ client/src/components/PointsBalance.js
@@ -4,7 +4,7 @@ import { Paper, Typography } from '@mui/material';
 const PointsBalance = ({ points }) => {
   return (
     <Paper elevation={2} sx={{ padding: 2, marginBottom: 2 }}>
-      <Typography variant="h6">Points Balance: {points}</Typography>
+      <Typography variant="h6">Points Balance: {points} NIL</Typography>
     </Paper>
   );
 };
