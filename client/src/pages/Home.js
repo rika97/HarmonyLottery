8d11f27026dc80434d3ae401803345c8d45a32dd
Rika <15hatoyar@gmail.com>
2024-09-12 15:34:23 -0700

    fix initialize user bug

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Typography } from '@mui/material';
 
 const Home = () => {
   const [points, setPoints] = useState(0);
 
-  useEffect(() => {
-    if (window.Telegram && window.Telegram.WebApp) {
-      const userId = window.Telegram.WebApp.initDataUnsafe.user.id;
-      initializeUser(userId).then(() => fetchPoints(userId));
-    } else {
-      console.error('Telegram WebApp is not available.');
-    }
-  }, []);
-
-  const initializeUser = async (userId) => {
+  const initializeUser = useCallback(async (userId) => {
     try {
       const response = await fetch(`https://hod1-a52bc53a961e.herokuapp.com/points?userId=${userId}`);
       const data = await response.json();
+
       if (data.points === undefined) {
-        await fetch('https://hod1-a52bc53a961e.herokuapp.com/initializeUser', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ userId }),
-        });
+        await fetch(`https://hod1-a52bc53a961e.herokuapp.com/initializeUser?userId=${userId}`, { method: 'POST' });
+        fetchPoints(userId);
+      } else {
+        setPoints(data.points);
       }
     } catch (error) {
       console.error('Error initializing user:', error);
     }
-  };
+  }, []);
+
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      const userId = window.Telegram.WebApp.initDataUnsafe.user.id;
+      initializeUser(userId);
+    } else {
+      console.error('Telegram WebApp is not available.');
+    }
+  }, [initializeUser]);
 
   const fetchPoints = async (userId) => {
     try {
