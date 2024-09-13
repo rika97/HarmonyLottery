51d55b45246d5579584be4dce47828409e1f1004
Rika <15hatoyar@gmail.com>
2024-09-12 21:29:58 -0700

    change appbar ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { Container, Typography, Box, AppBar, Toolbar, BottomNavigation, BottomNavigationAction } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import TaskIcon from '@mui/icons-material/List';
@@ -9,7 +9,27 @@ import Home from './pages/Home';
 import Tasks from './pages/Tasks';
 import Leaderboard from './pages/Leaderboard';
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case '/':
+        setValue(0);
+        break;
+      case '/tasks':
+        setValue(1);
+        break;
+      case '/leaderboard':
+        setValue(2);
+        break;
+      default:
+        setValue(0);
+        break;
+    }
+  }, [location]);
+
   return (
     <Router>
       <AppBar position="static">
@@ -22,6 +42,9 @@ function App() {
       
       <Container maxWidth="sm">
         <Box textAlign="center" my={4}>
+          <Typography variant="h3" gutterBottom>
+            Hod1
+          </Typography>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/tasks" element={<Tasks />} />
@@ -31,6 +54,8 @@ function App() {
       </Container>
 
       <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => setValue(newValue)}
         showLabels
         style={{
           width: '100%',
@@ -47,6 +72,6 @@ function App() {
       </BottomNavigation>
     </Router>
   );
-}
+};
 
 export default App;
