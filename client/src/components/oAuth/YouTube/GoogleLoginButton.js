568c6e76cbf0b496f0c463f56645578fba2b3b13
Rika <15hatoyar@gmail.com>
2024-09-14 12:45:59 -0700

    redirect bug

--- client/src/components/oAuth/YouTube/GoogleLoginButton.js
+++ client/src/components/oAuth/YouTube/GoogleLoginButton.js
@@ -6,7 +6,8 @@ const GoogleLoginButton = () => {
   const handleLoginClick = () => {
     const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
     const redirectUri = 'https://hod1.netlify.app/auth/google/callback';
-    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleClientId}&redirect_uri=${redirectUri}&response_type=token&scope=https://www.googleapis.com/auth/youtube.readonly`;
+
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleClientId}&redirect_uri=${redirectUri}&response_type=token&scope=https://www.googleapis.com/auth/youtube.readonly&prompt=select_account`;
 
     setDebugMessage('Button clicked');
 
