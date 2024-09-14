2fcf4df8c3cc656dc14b3ae7407c3d8333a7609b
Rika <15hatoyar@gmail.com>
2024-09-14 12:12:53 -0700

    redirect bug

--- client/src/components/oAuth/YouTube/GoogleLoginButton.js
+++ client/src/components/oAuth/YouTube/GoogleLoginButton.js
@@ -6,8 +6,13 @@ const GoogleLoginButton = () => {
     const redirectUri = 'https://hod1.netlify.app/auth/google/callback';
     const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleClientId}&redirect_uri=${redirectUri}&response_type=token&scope=https://www.googleapis.com/auth/youtube.readonly`;
 
-    window.location.href = googleAuthUrl;
+    if (window.Telegram && window.Telegram.WebApp) {
+      window.Telegram.WebApp.open(googleAuthUrl);
+    } else {
+      window.location.href = googleAuthUrl;
+    }
   };
+  
 
   return (
     <button onClick={handleLoginClick}>
