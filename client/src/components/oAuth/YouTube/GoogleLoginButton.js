26a3c85cc043f15b00e5fe30757a04c8a63bd7ec
Rika <15hatoyar@gmail.com>
2024-09-14 12:06:14 -0700

    redirect bug

--- client/src/components/oAuth/YouTube/GoogleLoginButton.js
+++ client/src/components/oAuth/YouTube/GoogleLoginButton.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
 
-const GoogleLoginButton = ({ onSuccess }) => {
-  const handleLoginSuccess = (credentialResponse) => {
-    const token = credentialResponse.credential;
-    window.location.href = `/auth/google/callback?access_token=${token}`;
+const GoogleLoginButton = () => {
+  const handleLoginClick = () => {
+    const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+    const redirectUri = 'https://hod1.netlify.app/auth/google/callback';
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleClientId}&redirect_uri=${redirectUri}&response_type=token&scope=https://www.googleapis.com/auth/youtube.readonly`;
+
+    window.location.href = googleAuthUrl;
   };
 
   return (
-    <GoogleLogin
-      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-      ux_mode="redirect"
-      onSuccess={handleLoginSuccess}
-      onError={(error) => console.error('Login Failed:', error)}
-    />
+    <button onClick={handleLoginClick}>
+      Login with Google
+    </button>
   );
 };
 
